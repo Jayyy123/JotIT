f67429a3c111a8cdc8eac0c23029ef93274807fd
Jokeseph <josephofilii@gmail.com>
2022-03-31 17:59:01 +0100

    JotIT - Rest Api for adding and deleting notes and also verifying users built and implemented on this website using Django.

--- JotIT/settings.py
+++ JotIT/settings.py
@@ -127,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
     BASE_DIR/'static'
 ]
@@ -134,6 +135,7 @@ MEDIA_URL = '/images/'
 MEDIA_ROOT = [
     BASE_DIR/'static/images'
 ]
+STATIC_ROOT = BASE_DIR/'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
